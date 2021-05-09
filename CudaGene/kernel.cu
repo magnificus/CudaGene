fd99a380c7c1f0ebd129d954e1116d5fa8917537
Tobias Elinder <tobiaselinder@gmail.com>
2021-05-09 21:44:37 +0200

    c++ cuda

---
+++ CudaGene/kernel.cu
@@ -0,0 +1,22 @@
+﻿#pragma once
+#include "cuda_runtime.h"
+#include "device_launch_parameters.h"
+
+#include <stdio.h>
+
+cudaError_t addWithCuda(int *c, const int *a, const int *b, unsigned int size);
+
+
+
+__global__ void addKernel(int *c, const int *a, const int *b)
+{
+    int i = threadIdx.x;
+    c[i] = a[i] - b[i];
+}
+
+
+extern "C" void
+launch_addkernel(int size, int *c, const int *a, const int *b) {
+    addKernel << <1, size >> > (c, a, b);
+}
+
